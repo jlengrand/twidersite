90ad46c9ccecc329cda64c0d3d93fb14b3975b6e
Julien Lengrand-Lambert <julien@lengrand.fr>
2013-02-03 19:14:52 +0100

    Attempts to move leaderboard

--- flask_app.py
+++ flask_app.py
@@ -6,7 +6,7 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world(name=None):
+@app.route('/leader')
+def leader(name=None):
     #return 'Hello from Flask!'
     return render_template('-static/leader.html', name=name)
